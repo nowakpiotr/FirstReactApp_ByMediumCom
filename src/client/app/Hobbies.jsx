29f9d3676dc45ec03d05825ab0bc22176a61b468
piotr <en.piotr@gmail.com>
2016-08-24 19:21:27 +0200

    Hello word

---
+++ src/client/app/Hobbies.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class Hobbies extends React.Component {
+  render () {
+    return <div>
+                <p> Hello React!</p>
+            </div>;
+  }
+}
+export default Hobbies;
--- src/client/app/Profile.jsx
+++ src/client/app/Profile.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
-import {render} from 'react-dom';
 
-
-
-class App extends React.Component {
+class Profile extends React.Component {
   render () {
     return <div>
                 <p> Hello React!</p>
-
             </div>;
   }
 }
 
-render(<App />, document.getElementById('app'));
+export default Profile;
